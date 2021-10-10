c8f6862bcecdeb58e630212d5d80f8c8a6fb7d11
Kieran Smith <imkieransmith@users.noreply.github.com>
2021-10-10 18:48:20 +0100

    Minify assets

--- resources/views/app/view-day.blade.php
+++ resources/views/app/view-day.blade.php
@@ -5,19 +5,27 @@
 
             <h3 class="text-xl">Wellness</h3>
             <p class="text-xs pb-2 mb-2 border-b border-gray-100">How you're planning to start your morning.</p>
-            <div class="md:flex items-start justify-between mb-6">
-                <p class="mr-4 mb-2 md:mb-0">{{ $day->wellness }}</p>
-                <button type="button" class="flex items-center justify-start text-sm hover:underline whitespace-nowrap ml-auto"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-600 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg> Mark as completed</button>
-            </div>
+            @if($day->wellness)
+                <div class="md:flex items-start justify-between mb-6">
+                    <p class="mr-4 mb-2 md:mb-0">{{ $day->wellness }}</p>
+                    <button type="button" class="flex items-center justify-start text-sm hover:underline whitespace-nowrap ml-auto"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-600 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg> Mark as completed</button>
+                </div>
+            @else
+                <p class="mb-6">No wellness task has been set.</p>
+            @endif
 
             <h3 class="text-xl">Goals</h3>
             <p class="text-xs pb-2 mb-2 border-b border-gray-100">What you're planning to achieve with your morning.</p>
-            @foreach($day->goals as $goal)
-                <div class="md:flex items-start justify-between mb-6">
-                    <p class="mr-4 mb-2 md:mb-0">{{ $goal->aim }}</p>
-                    <button type="button" class="flex items-center justify-start text-sm hover:underline whitespace-nowrap ml-auto"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-600 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg> Mark as completed</button>
-                </div>
-            @endforeach
+            @if(count($day->goals))
+                @foreach($day->goals as $goal)
+                    <div class="md:flex items-start justify-between mb-6">
+                        <p class="mr-4 mb-2 md:mb-0">{{ $goal->aim }}</p>
+                        <button type="button" class="flex items-center justify-start text-sm hover:underline whitespace-nowrap ml-auto"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-600 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" /></svg> Mark as completed</button>
+                    </div>
+                @endforeach
+            @else
+                <p class="mb-6">No goals have been set.</p>
+            @endif
 
             <div class="flex items-center justify-between">
                 <a
